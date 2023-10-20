4c9688c34219cb6a7c336e87a82cc2499442e8bc
Ayda Ghalkhanbaz <aydag997@gmail.com>
2023-10-20 23:03:54 +0200

    Done

--- Assignment8/OAHash.java
+++ Assignment8/OAHash.java
@@ -2,12 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
 
-/* this version handles collisions. 
-* updated by adding an attribute next pointer into the Node class
-* in the collision spots, all nodes with same hash are linked to 
-* each other thanks to the next pointer*/
+/* this version handles collisions using open addressing. 
+* if there are collisions, then it adds the item at the
+* next empty spot.*/
 
-public class BetterHash{
+public class OAHash{
     Node[] data;
     static int[] keys;
     int max;
@@ -17,17 +16,15 @@ public class BetterHash{
         Integer code;
         String name;
         Integer pop;
-        Node next; // new
         
         public Node(Integer code, String name, Integer pop){
             this.code = code;
             this.name = name;
             this.pop = pop;
-            next = null;
         }
     }
 
-    public BetterHash(String file , int mod){
+    public OAHash(String file , int mod){
         this.mod = mod;
         data = new Node[mod];
         keys = new int[9676];
@@ -43,45 +40,29 @@ public class BetterHash{
             }
             max = i-1;
         }catch(Exception e){
-            System.out.println("File " + file + " not found!");
+            System.out.println("File " + file + " not found! " + e);
         }
     }
-    /* adds a node in its hashed spot. 
-    collision? next pointer of the previous one points to the new one*/
+    /* collisions? find the next empty spot!*/
     public void add(Integer code, Node n){
         Integer hashKey = code%mod;
-        if(data[hashKey] == null){
-            data[hashKey] = n;
+        while(data[hashKey] != null){
+            hashKey++;
         }
-        else{
-            Node curr = data[hashKey];
-            while(curr.next != null){
-                curr = curr.next;
-            }
-            curr.next = n;
-        }
+        data[hashKey] = n;
+        
     }
-    // collision? we check all next nodes till we find it
+
     public String lookup(Integer zip){
 
         Integer hashKey = zip%mod;
         int number = 0;
-
-        if(data[hashKey].code.equals(zip)){
-            System.out.println("the number of elements to check before: "+ number);
-            return data[hashKey].name;
-        }else{
-            Node curr = data[hashKey];
-            while(curr != null){
-                if(curr.code.equals(zip)){
-                    System.out.println("the number of elements to check before: "+ number);
-                    return curr.name;
-                }
-                number++;
-                curr = curr.next;
-            }
-            return null;
+        while(!(data[hashKey].code.equals(zip))){
+            hashKey++;
+            number++;
         }
+        System.out.println("the step counter: " + number);
+        return data[hashKey].name;
     }
 
     public void collisions(int mod){
@@ -100,19 +81,18 @@ public class BetterHash{
     }
 
     public static void main(String[] args){
-        int[] mods = {10000,12345,13513,13600,14000,20000,30000,40000};
-        //int[] mods = {31327};
+        int[] mods = {13513,20000,30000,31327,40000};
+        Random rnd = new Random();
+        int r = rnd.nextInt(9676);
        System.out.println("\n5. slightly better");
         for(int m: mods){
             System.out.println();
-            System.out.println("Array size: " + m);
-            BetterHash h = new BetterHash("postnummer.csv", m);
-            Random rnd = new Random();
-            Integer k = keys[rnd.nextInt(9676)];
-            System.out.println("The Post Code: " + k);
+            System.out.println("Array size/mod: " + m);
+            OAHash h = new OAHash("postnummer.csv", m);
+            Integer k = keys[r];
+            System.out.println("The Post Code: " + k );
             String s = h.lookup(k);
             System.out.println("City: " + s);
         }
     }
 }
-
